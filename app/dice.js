fec5628616c4b1a8f631ff72760212df9912353c
Mitchell Amihod <mitchell.amihod@gmail.com>
2014-08-27 08:53:32 -0400

    Allow dice.roll to take in NdX string as well, since we will start bundling the roll type with the data tables

--- app/dice.js
+++ app/dice.js
@@ -10,11 +10,19 @@ define(function(require, exports, module) {
 
   /**
    *
-   * @param  {int} type  sides of dice
-   * @param  {int} [times=1] how many rolls?
+   * @param  {int|string} type string: NdX, eg: 4d6, 3d4
+   *                           int: sidedness of dieint: type sides of dice
+   * @param  {int} [times=1] how many rolls
    * @return {int}
    */
   dice.roll = function roll(type, times) {
+
+    if(typeof type === 'string') {
+      var NdX = type.split('d');
+      times = NdX[0];
+      type = NdX[1];
+    }
+
     times = times || 1;
 
     var total = 0;
